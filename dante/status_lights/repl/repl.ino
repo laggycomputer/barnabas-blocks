a9b5d848ed511ff70c1e3bfc4fbf1015a03b32b9
Dante Dam <laggycomputer@yahoo.com>
2021-02-17 20:16:34 -0800

    My changes for the week. All I know is that I am too tired to explain them all here

--- dante/status_lights/repl/repl.ino
+++ dante/status_lights/repl/repl.ino
@@ -1,31 +1,90 @@
-#define update_hz 5  // How many times to broadcast pins per second
 #include <limits.h>
 
 const int digitals[14] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-const int analogs[6] = {A0, A1, A2, A3, A4, A5};
+const int analogs[6] = {A0, A1, A2, A3, A4, A5};  // Really just aliases to 14, 15, 16, 17, 18, and 19 but readability
+
+// 0 is input to Ardiuno, 1 is output from Arduino
+// Despite the fact that we want 0 to always be input and 1 and 13 to always be output, leave the array at the same size for consistency
+// analogWrite only works on analog and PWM pins, so let's use digital for now
+int digital_io_states[14] = {0};
+int analog_io_states[6] = {0};
+
+char incomingByte = 0;
+String incomingStuff = "";
+int stuff_dump = -1;
+int new_state = -1;
+int pin_to_change = -1;
 
 void setup() {
-  for (int pin = 0; pin < 14; pin++) {
-    pinMode(digitals[pin], INPUT);
-  }
+    for (int pin = 0; pin < 14; pin++) {
+        pinMode(digitals[pin], INPUT);
+    }
 
-  for (int pin = 0; pin < 6; pin++) {
-    pinMode(analogs[pin], INPUT);
-  }
-  Serial.begin(19200);
+    for (int pin = 0; pin < 6; pin++) {
+        pinMode(analogs[pin], INPUT);
+    }
+    Serial.begin(19200);
+    Serial.setTimeout(5000);
+}
+
+void sendUpdate() {
+    for (int digital = 0; digital < 14; digital++) {
+        if (!digital_io_states[digital]) {
+            Serial.print(digitalRead(digitals[digital]));
+        } else {
+            Serial.print("?");
+        }
+        Serial.print(" ");
+    }
+
+    for (int analog = 0; analog < 6; analog++) {
+        if (!analog_io_states[analog]) {
+            Serial.print(analogRead(analogs[analog]));
+            Serial.print(" ");
+        }
+    }
+
+    Serial.print("\n");
 }
 
 void loop() {
-  for (int digital = 0; digital < 14; digital++) {
-    Serial.print(digitalRead(digitals[digital]));
-    Serial.print(" ");
-  }
+    if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        if (incomingByte == 13 || incomingByte == 10) {
+            // Discard CR/LF
+            return;
+        }
 
-  for (int analog = 0; analog < 6; analog++) {
-    Serial.print(analogRead(analogs[analog]));
-    Serial.print(" ");
-  }
-
-  Serial.print("\n");
-  delay(1000 / update_hz);
+        switch (incomingByte) {
+            case 'g':
+                sendUpdate();
+                break;
+            case 'u':
+                incomingStuff = Serial.readStringUntil('\n');
+                stuff_dump = incomingStuff.toInt();
+                if (stuff_dump > 100) {
+                    new_state = 1;
+                    pin_to_change = stuff_dump - 100;
+                } else {
+                    new_state = 0;
+                    pin_to_change = stuff_dump;
+                }
+                if (pin_to_change < 0 || pin_to_change > 19) {
+                    return;
+                }
+                if (pin_to_change == 0 || pin_to_change == 1 || pin_to_change == 13) {  // Also invalid
+                    return;
+                }
+                pinMode(pin_to_change, new_state);
+                if (pin_to_change <= 13) {
+                    // This is a digital, update to digital state array.
+                    digital_io_states[pin_to_change] = new_state;
+                } else {
+                    analog_io_states[pin_to_change - 14] = new_state;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
--- dante/status_lights/script.js
+++ dante/status_lights/script.js
@@ -28,18 +28,36 @@ let outputStream;
 
 const log = document.getElementById('log');
 const butConnect = document.getElementById('butConnect');
+const butRefresh = document.getElementById('butRefresh');
+const butModeSubmit = document.getElementById('butModeSubmit');
 const digital_pin_names = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
 const analog_pin_names = ["A0", "A1", "A2", "A3", "A4", "A5"];
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  butConnect.addEventListener('click', clickConnect);
+    butConnect.addEventListener('click', clickConnect);
+    butRefresh.addEventListener('click', clickRefresh);
+    butModeSubmit.addEventListener('click', clickModeSubmit);
 
-  // CODELAB: Add feature detection here.
-  const notSupported = document.getElementById('notSupported');
-  notSupported.classList.toggle('hidden', 'serial' in navigator);
+    // CODELAB: Add feature detection here.
+    const notSupported = document.getElementById('notSupported');
+    notSupported.classList.toggle('hidden', 'serial' in navigator);
 });
 
+async function clickModeSubmit() {
+    if (port) {
+        const pin_to_update = document.getElementById("pinToChangeMode").value;
+        const state_to_set = document.getElementById("modeOptions").value;
+        await writeToStream("u\r", state_to_set + pin_to_update + "\n");
+    }
+}
+
+
+async function clickRefresh() {
+    if (port) {
+        await writeToStream("g\n");
+    }
+}
 
 /**
  * @name connect
@@ -47,24 +65,24 @@ document.addEventListener('DOMContentLoaded', () => {
  * output stream.
  */
 async function connect() {
-  // CODELAB: Add code to request & open port here.
-  // - Request a port and open a connection.
-  port = await navigator.serial.requestPort();
-  // - Wait for the port to open.
-  await port.open({baudRate: 19200});
-  // CODELAB: Add code setup the output stream here.
-  const encoder = new TextEncoderStream();
-  outputDone = encoder.readable.pipeTo(port.writable);
-  outputStream = encoder.writable;
-  // CODELAB: Add code to read the stream here.
-  let decoder = new TextDecoderStream();
-  inputStream = decoder.readable
+    // CODELAB: Add code to request & open port here.
+    // - Request a port and open a connection.
+    port = await navigator.serial.requestPort();
+    // - Wait for the port to open.
+    await port.open({baudRate: 19200});
+    // CODELAB: Add code setup the output stream here.
+    const encoder = new TextEncoderStream();
+    outputDone = encoder.readable.pipeTo(port.writable);
+    outputStream = encoder.writable;
+    // CODELAB: Add code to read the stream here.
+    let decoder = new TextDecoderStream();
+    inputStream = decoder.readable
     .pipeThrough(new TransformStream(new LineBreakTransformer()));
-  inputDone = port.readable.pipeTo(decoder.writable);
-  reader = inputStream.getReader();
-  readLoop();
-  // The codelab wants us to decode via JSON as well, though that isn't needed here
-  // .pipeThrough(new TransformStream(new JSONTransformer()));
+    inputDone = port.readable.pipeTo(decoder.writable);
+    reader = inputStream.getReader();
+    readLoop();
+    // The codelab wants us to decode via JSON as well, though that isn't needed here
+    // .pipeThrough(new TransformStream(new JSONTransformer()));
 }
 
 /**
@@ -72,102 +90,99 @@ async function connect() {
  * Closes the Web Serial connection.
  */
 async function disconnect() {
-  // CODELAB: Close the input stream (reader).
-  if (reader) {
-    await reader.cancel();
-    await inputDone.catch(() => {});
-    reader = null;
-    inputDone = null;
-  }
-  // CODELAB: Close the output stream.
-  if (outputStream) {
-    await outputStream.getWriter().close();
-    await outputDone;
-    outputStream = null;
-    outputDone = null;
-  }
-  // CODELAB: Close the port.
-  await port.close();
-  port = null;
+    // CODELAB: Close the input stream (reader).
+    if (reader) {
+        await reader.cancel();
+        await inputDone.catch(() => {});
+        reader = null;
+        inputDone = null;
+    }
+    // CODELAB: Close the output stream.
+    if (outputStream) {
+        await outputStream.getWriter().close();
+        await outputDone;
+        outputStream = null;
+        outputDone = null;
+    }
+    // CODELAB: Close the port.
+    await port.close();
+    port = null;
 }
 
 var str2ab = function (str) {
-  // var encodedString = unescape(encodeURIComponent(str));
-  var encodedString = str;
-  var bytes = new Uint8Array(encodedString.length);
-  for (var i = 0; i < encodedString.length; ++i) {
-    bytes[i] = encodedString.charCodeAt(i);
-  }
-  return bytes.buffer;
+    // var encodedString = unescape(encodeURIComponent(str));
+    var encodedString = str;
+    var bytes = new Uint8Array(encodedString.length);
+    for (var i = 0; i < encodedString.length; ++i) {
+        bytes[i] = encodedString.charCodeAt(i);
+    }
+    return bytes.buffer;
 };
 
 function flashCode(code, nano=false, options={}) {
-
-  if (nano) {
-    var board_to_api = "arduino:avr:nano:cpu=atmega328";
-    var board_to_upload = "nano";
-  } else {
-    var board_to_api = "arduino:avr:uno";
-    var board_to_upload = "uno";
-  }
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "repl/repl.ino", false);
-  xmlhttp.send();
-  if (xmlhttp.status == 200) {
-    result = xmlhttp.responseText;
-  }
-  var result = code;
-
-  var data = { sketch: result, board: board_to_api };
-
-  fetch(
-    "https://compile.barnabasrobotics.com/compile", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-     }
-  ).then(response => response.json()).then(data => {
-   // console.log(data);
-   if (!data.success) {
-     // can only run below if arduino compile error I can still get response with garbage body
-     if (data.stderr.length > 0) {
-       let regex = /\/tmp\/chromeduino\-(.*?)\/chromeduino\-(.*?)\.ino\:/g;
-       let message = data.stderr.replace(regex, "");
-       uploadLog.textContent += "Compilation error:\n" + message + "\n"
-       regex = /\d+\:\d+/g;
-     }
-   } else {
-     let hexstring = atob(data.hex);
-     return { 'data': hexstring, 'msg': data.stdout };
-   }
- }
- ).then(hex => {
-    if (hex) {
-      try {
-        var avrgirl = new AvrgirlArduino({
-          board: board_to_upload,
-          debug: false
-        });
-
-        avrgirl.flash(str2ab(hex.data), (error) => {
-          // gear.classList.remove('spinning');
-          // progress.textContent = "done!";
-          if (error) {
-            uploadLog.textContent += "Upload error:\n" + error + "\n";
-            avrgirl.connection.serialPort.close();
-          } else {
-            uploadLog.textContent += "Upload successful.\n";
-          }
-        }, options);
-      } catch (error) {
-        uploadLog.textContent += "AVR error:\n" + error + "\n";
-        avrgirl.connection.serialPort.close();
-      }
+    if (nano) {
+        var board_to_api = "arduino:avr:nano:cpu=atmega328";
+        var board_to_upload = "nano";
+    } else {
+        var board_to_api = "arduino:avr:uno";
+        var board_to_upload = "uno";
     }
-  });
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "repl/repl.ino", false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+        result = xmlhttp.responseText;
+    }
+    var result = code;
+
+    var data = { sketch: result, board: board_to_api };
+
+    fetch(
+    "https://compile.barnabasrobotics.com/compile", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        }
+    ).then(response => response.json()).then(data => {
+        if (!data.success) {
+            // can only run below if arduino compile error I can still get response with garbage body
+            if (data.stderr.length > 0) {
+                let regex = /\/tmp\/chromeduino\-(.*?)\/chromeduino\-(.*?)\.ino\:/g;
+                let message = data.stderr.replace(regex, "");
+                uploadLog.textContent += "Compilation error:\n" + message + "\n"
+                regex = /\d+\:\d+/g;
+            }
+        } else {
+            let hexstring = atob(data.hex);
+            return { 'data': hexstring, 'msg': data.stdout };
+        }
+    }).then(hex => {
+        if (hex) {
+            try {
+                var avrgirl = new AvrgirlArduino({
+                    board: board_to_upload,
+                    debug: false
+                });
+
+                avrgirl.flash(str2ab(hex.data), (error) => {
+                    // gear.classList.remove('spinning');
+                    // progress.textContent = "done!";
+                    if (error) {
+                        uploadLog.textContent += "Upload error:\n" + error + "\n";
+                        avrgirl.connection.serialPort.close();
+                    } else {
+                        uploadLog.textContent += "Upload successful.\n";
+                    }
+                }, options);
+            } catch (error) {
+                uploadLog.textContent += "AVR error:\n" + error + "\n";
+                avrgirl.connection.serialPort.close();
+            }
+        }
+    });
 }
 
 /**
@@ -175,16 +190,16 @@ function flashCode(code, nano=false, options={}) {
  * Click handler for the connect/disconnect button.
  */
 async function clickConnect() {
-  // CODELAB: Add disconnect code here.
-  if (port) {
-    await disconnect();
-    toggleUIConnected(false);
-    return;
-  }
-  // CODELAB: Add connect code here.
-  await connect();
+    // CODELAB: Add disconnect code here.
+    if (port) {
+        await disconnect();
+        toggleUIConnected(false);
+        return;
+    }
+    // CODELAB: Add connect code here.
+    await connect();
 
-  toggleUIConnected(true);
+    toggleUIConnected(true);
 }
 
 
@@ -193,47 +208,56 @@ async function clickConnect() {
  * Reads data from the input stream and displays it on screen.
  */
 async function readLoop() {
-  // CODELAB: Add read loop here.
-  while (true) {
-    const { value, done } = await reader.read();
+    // CODELAB: Add read loop here.
+    while (true) {
+        const { value, done } = await reader.read();
 
-    let bit_chunks = value.trim().split(" ");
-    let digitals = bit_chunks.slice(0, 14);
-    let analogs = bit_chunks.slice(14);
+        let bit_chunks = value.trim().split(" ");
+        let digitals = bit_chunks.slice(0, 14);
+        let analogs = bit_chunks.slice(14);
 
-    for (let digital_pin_number = 0; digital_pin_number < 14; digital_pin_number++) {
-        let state = parseInt(digitals[digital_pin_number]);
-        let elem_to_update = document.getElementById("bool" + digital_pin_names[digital_pin_number]);
-        if (state) {
-            elem_to_update.src = "assets/on.svg"
-        } else {
-            elem_to_update.src = "assets/off.svg"
+        for (let digital_pin_number = 0; digital_pin_number < 14; digital_pin_number++) {
+            let elem_to_update = document.getElementById("bool" + digital_pin_names[digital_pin_number]);
+            if (digitals[digital_pin_number] != "?") {
+                let state = parseInt(digitals[digital_pin_number]);
+                if (state) {
+                    elem_to_update.src = "assets/on.svg";
+                } else {
+                    elem_to_update.src = "assets/off.svg";
+                }
+            } else {
+              elem_to_update.src = "assets/unknown.svg"
+            }
+        }
+
+        for (let analog_pin_number = 0; analog_pin_number < 6; analog_pin_number++) {
+            let elem_to_update = document.getElementById("bool" + analog_pin_names[analog_pin_number]);
+            if (analogs[analog_pin_number] != "?") {
+                let state = parseInt(analogs[analog_pin_number]);
+                if (state > 1023) {
+                    state = 1023;
+                } else if (state < 0) {
+                    state = 0;
+                }
+                if (state) {
+                    elem_to_update.src = "assets/on.svg"
+                } else {
+                    elem_to_update.src = "assets/off.svg"
+                }
+                state = (state + 1) / 1024 * 5;
+                document.getElementById(analog_pin_names[analog_pin_number]).innerHTML = state.toFixed(2).toString();
+            } else {
+                elem_to_update.src = "assets/unknown.svg";
+                document.getElementById(analog_pin_names[analog_pin_number]).innerHTML = "(pin in output mode)";
+            }
+        }
+
+        if (done) {
+            console.log('[readLoop] DONE', done);
+            reader.releaseLock();
+            break;
         }
     }
-
-    for (let analog_pin_number = 0; analog_pin_number < 6; analog_pin_number++) {
-        let state = parseInt(analogs[analog_pin_number]);
-        let elem_to_update = document.getElementById("bool" + analog_pin_names[analog_pin_number]);
-        if (state > 1023) {
-          state = 1023;
-        } else if (state < 0) {
-          state = 0;
-        }
-        if (state) {
-            elem_to_update.src = "assets/on.svg"
-        } else {
-            elem_to_update.src = "assets/off.svg"
-        }
-        state = (state + 1) / 1024 * 5;
-        document.getElementById(analog_pin_names[analog_pin_number]).innerHTML = state.toFixed(2).toString();
-    }
-
-    if (done) {
-      console.log('[readLoop] DONE', done);
-      reader.releaseLock();
-      break;
-    }
-  }
 }
 
 
@@ -243,13 +267,13 @@ async function readLoop() {
  * @param  {...string} lines lines to send to the micro:bit
  */
 function writeToStream(...lines) {
-  // CODELAB: Write to output stream
-  const writer = outputStream.getWriter();
-  lines.forEach((line) => {
-    console.log('[SEND]', line);
-    writer.write(line + '\n');
-  });
-  writer.releaseLock();
+    // CODELAB: Write to output stream
+    const writer = outputStream.getWriter();
+    lines.forEach((line) => {
+        console.log('[SEND]', line);
+        writer.write(line); // Allow user to specify which line ending
+    });
+    writer.releaseLock();
 }
 
 /**
@@ -257,23 +281,23 @@ function writeToStream(...lines) {
  * TransformStream to parse the stream into lines.
  */
 class LineBreakTransformer {
-  constructor() {
+    constructor() {
     // A container for holding stream data until a new line.
-    this.container = '';
-  }
+        this.container = '';
+    }
 
-  transform(chunk, controller) {
+    transform(chunk, controller) {
     // CODELAB: Handle incoming chunk
-    this.container += chunk;
-    const lines = this.container.split('\n');
-    this.container = lines.pop();
-    lines.forEach(line => controller.enqueue(line));
-  }
+        this.container += chunk;
+        const lines = this.container.split('\n');
+        this.container = lines.pop();
+        lines.forEach(line => controller.enqueue(line));
+    }
 
-  flush(controller) {
+    flush(controller) {
     // CODELAB: Flush the stream.
-    controller.enqueue(this.container);
-  }
+        controller.enqueue(this.container);
+    }
 }
 
 
@@ -282,14 +306,14 @@ class LineBreakTransformer {
  * TransformStream to parse the stream into a JSON object.
  */
 class JSONTransformer {
-  transform(chunk, controller) {
+    transform(chunk, controller) {
     // CODELAB: Attempt to parse JSON content
     try {
-      controller.enqueue(JSON.parse(chunk));
+        controller.enqueue(JSON.parse(chunk));
     } catch (e) {
-      controller.enqueue(chunk);
+        controller.enqueue(chunk);
     }
-  }
+    }
 }
 
 /**
@@ -297,9 +321,9 @@ class JSONTransformer {
  */
 
 function toggleUIConnected(connected) {
-  let lbl = 'Connect';
-  if (connected) {
-    lbl = 'Disconnect';
-  }
-  butConnect.textContent = lbl;
+    let lbl = 'Connect';
+    if (connected) {
+        lbl = 'Disconnect';
+    }
+    butConnect.textContent = lbl;
 }
